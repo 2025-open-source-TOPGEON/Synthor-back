f437a7e83d9b9a9d43823a21655a3e8d547155f6
gundoan <gunubu01@gmail.com>
2025-07-17 21:59:27 +0900

    fix : 한국버전이랑 영어버전을 나눔

--- backend/Synthor_backend/src/main/java/com/synthor/backend/service/DataGenerationService.java
+++ backend/Synthor_backend/src/main/java/com/synthor/backend/service/DataGenerationService.java
@@ -44,6 +44,7 @@ public class DataGenerationService {
 
     private Object generateValueByType(String type) {
         return switch (type) {
+<<<<<<< Updated upstream
             // --- Korean-context data (uses koreanFaker) ---
             case "full_name" -> koreanFaker.name().fullName();
             case "first_name" -> koreanFaker.name().firstName();
@@ -66,38 +67,107 @@ public class DataGenerationService {
             case "color" -> koreanFaker.color().name();
             case "catch_phrase" -> koreanFaker.company().catchPhrase();
             case "paragraphs" -> String.join("\n\n", koreanFaker.lorem().paragraphs(3));
+=======
+            // --- [KOREAN] Person & Personal Info ---
+            case "korean_full_name" -> koreanFaker.name().lastName() + koreanFaker.name().firstName();
+            case "korean_first_name" -> koreanFaker.name().firstName();
+            case "korean_last_name" -> koreanFaker.name().lastName();
+            case "korean_phone" -> koreanFaker.phoneNumber().cellPhone();
 
-            // --- English/Default-context data (uses defaultFaker) ---
+            // --- [ENGLISH] Person & Personal Info ---
+            case "full_name" -> defaultFaker.name().fullName();
+            case "first_name" -> defaultFaker.name().firstName();
+            case "last_name" -> defaultFaker.name().lastName();
+            case "phone" -> defaultFaker.phoneNumber().cellPhone();
+>>>>>>> Stashed changes
+
+            // --- [KOREAN] Address ---
+            case "korean_address" -> koreanFaker.address().fullAddress();
+            case "korean_street_address" -> koreanFaker.address().streetAddress();
+            case "korean_city" -> koreanFaker.address().city();
+            case "korean_state" -> koreanFaker.address().state();
+            case "korean_country" -> koreanFaker.address().country();
+            case "korean_postal_code" -> koreanFaker.address().zipCode();
+
+            // --- [ENGLISH] Address ---
+            case "address" -> defaultFaker.address().fullAddress();
+            case "street_address" -> defaultFaker.address().streetAddress();
+            case "city" -> defaultFaker.address().city();
+            case "state" -> defaultFaker.address().state();
+            case "country" -> defaultFaker.address().country();
+            case "postal_code" -> defaultFaker.address().zipCode();
+
+            // --- [KOREAN] Company & Commerce ---
+            case "korean_company_name" -> koreanFaker.company().name();
+            case "korean_job_title" -> koreanFaker.job().title();
+            case "korean_department_corporate" -> koreanFaker.company().profession();
+            case "korean_department_retail" -> koreanFaker.commerce().department();
+            case "korean_product_name" -> koreanFaker.commerce().productName();
+            case "korean_product_category" -> koreanFaker.commerce().department();
+            case "korean_catch_phrase" -> koreanFaker.company().catchPhrase();
+            case "korean_product_description" -> String.join(" ", koreanFaker.lorem().sentences(2));
+
+            // --- [ENGLISH] Company & Commerce ---
+            case "company_name" -> defaultFaker.company().name();
+            case "job_title" -> defaultFaker.job().title();
+            case "department_corporate" -> defaultFaker.company().profession();
+            case "department_retail" -> defaultFaker.commerce().department();
+            case "product_name" -> defaultFaker.commerce().productName();
+            case "product_category" -> defaultFaker.commerce().department();
+            case "catch_phrase" -> defaultFaker.company().catchPhrase();
+            case "product_description" -> String.join(" ", defaultFaker.lorem().sentences(2));
+
+            // --- [KOREAN] Finance ---
+            case "korean_bank_name" -> koreanFaker.company().name();
+
+            // --- [ENGLISH] Finance ---
+            case "bank_name" -> defaultFaker.finance().bank();
+
+            // --- [KOREAN] Misc ---
+            case "korean_language" -> koreanFaker.nation().language();
+            case "korean_color" -> koreanFaker.color().name();
+
+            // --- [ENGLISH] Misc ---
+            case "language" -> defaultFaker.nation().language();
+            case "color" -> defaultFaker.color().name();
+
+            // =======================================================================================
+            // --- LOCALE-NEUTRAL / ENGLISH-DEFAULT TYPES (No Korean version needed) ---
+            // =======================================================================================
+
+            // --- Internet & Tech ---
             case "username" -> defaultFaker.name().username();
             case "password" -> defaultFaker.internet().password();
-            case "email_address" -> generateCustomEmail(); // Use custom email generator
+            case "email_address" -> generateCustomEmail();
             case "domain_name" -> defaultFaker.internet().domainName();
             case "url" -> defaultFaker.internet().url();
-            case "app_name" -> defaultFaker.app().name();
-            case "app_version" -> defaultFaker.app().version();
             case "mac_address" -> defaultFaker.internet().macAddress();
             case "ip_v4_address" -> defaultFaker.internet().ipV4Address();
             case "ip_v6_address" -> defaultFaker.internet().ipV6Address();
             case "user_agent" -> defaultFaker.internet().userAgent();
-            case "credit_card_type" -> defaultFaker.business().creditCardType();
+            case "avatar" -> defaultFaker.avatar().image();
+
+            // --- App & Device ---
+            case "app_name" -> defaultFaker.app().name();
+            case "app_version" -> defaultFaker.app().version();
             case "device_model" -> defaultFaker.device().modelName();
             case "device_brand" -> defaultFaker.device().manufacturer();
             case "device_os" -> defaultFaker.device().platform();
-            case "avatar" -> defaultFaker.avatar().image();
 
-            // --- Locale-neutral data (can use either faker) ---
-            case "phone" -> koreanFaker.phoneNumber().cellPhone(); // Korean phone format is better
-            case "postal_code" -> koreanFaker.address().zipCode(); // Korean postal code format
-            case "latitude" -> defaultFaker.address().latitude();
-            case "longitude" -> defaultFaker.address().longitude();
-            case "product_price" -> defaultFaker.commerce().price();
+            // --- Finance ---
             case "credit_card_number" -> defaultFaker.finance().creditCard();
-            case "bank_routing_number" -> defaultFaker.number().digits(9);
+            case "credit_card_type" -> defaultFaker.business().creditCardType();
+            case "product_price" -> defaultFaker.commerce().price();
+            case "currency" -> defaultFaker.currency().name();
             case "iban" -> defaultFaker.finance().iban();
             case "swift_bic" -> defaultFaker.finance().bic();
-            case "currency" -> defaultFaker.currency().name();
+
+            // --- Misc ---
+            case "paragraphs" -> String.join("\n\n", defaultFaker.lorem().paragraphs(3));
             case "datetime" -> defaultFaker.date().past(365, TimeUnit.DAYS).toString();
             case "time" -> DateTimeFormatter.ofPattern("HH:mm:ss").format(defaultFaker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalTime());
+            case "latitude" -> defaultFaker.address().latitude();
+            case "longitude" -> defaultFaker.address().longitude();
             case "number_between_1_100" -> defaultFaker.number().numberBetween(1, 101);
 
             default -> "Unsupported Type: " + type;
