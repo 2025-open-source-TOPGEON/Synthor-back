8e60347fb86239e10677cf477e0de2a22e4769da
gundoan <gunubu01@gmail.com>
2025-07-24 18:08:08 +0900

    feat : korean_address_line_2 데이터 타입을 추가함

--- backend/Synthor_backend/src/main/java/com/synthor/backend/service/DataGenerationService.java
+++ backend/Synthor_backend/src/main/java/com/synthor/backend/service/DataGenerationService.java
@@ -29,6 +29,32 @@ public class DataGenerationService {
     private static final String[] KOREAN_GENDERS = {"여자", "남자"};
     private static final String[] GENDERS_WITH_NON_BINARY = {"Female", "Male", "Non-binary"};
     private static final String[] KOREAN_GENDERS_WITH_NON_BINARY = {"여자", "남자", "그 외"};
+    private static final String[] ADDRESS_LINE_2_EXAMPLES = {
+            "Apt 1306", "Suite 34", "Room 327", "17th Floor", "PO Box 12055", "Apt 1233",
+            "Suite 44", "PO Box 88462", "Apt 340", "PO Box 41504", "Room 1262", "Room 312",
+            "Room 1699", "Room 1980", "Suite 19", "PO Box 71509", "Apt 275", "Room 1554",
+            "Apt 1016", "PO Box 36274", "PO Box 12632", "Suite 13", "PO Box 41496", "Suite 90",
+            "Apt 1570", "Suite 63", "14th Floor", "Suite 26", "5th Floor", "Apt 112", "Apt 539",
+            "PO Box 78946", "13th Floor", "4th Floor", "Room 1484", "Apt 1518", "Apt 178",
+            "Apt 1199", "Room 27", "PO Box 7916", "Room 950", "8th Floor", "Apt 1420",
+            "PO Box 50509", "Suite 92", "PO Box 97195", "Room 417", "PO Box 74379",
+            "PO Box 93574", "Apt 392", "Room 403", "Apt 919",
+            "Room 1109", "Room 1188", "Room 1215", "Room 97", "Room 889", "PO Box 20113",
+            "Room 374", "11th Floor", "9th Floor", "Suite 43", "PO Box 51543", "Suite 57",
+            "Room 770", "Apt 431", "PO Box 41967", "Room 189", "Suite 15", "PO Box 1326",
+            "Room 991", "Apt 1616", "Room 982", "Apt 1019", "Room 1842", "Suite 52",
+            "18th Floor", "Suite 11", "PO Box 68970",
+            "Suite 85", "Suite 67", "Room 285", "Room 1340", "15th Floor", "Room 727",
+            "PO Box 57575", "16th Floor", "Room 1373", "Room 1541",
+            "PO Box 7326", "Apt 767", "Suite 70"
+    };
+    private static final String[] KOREAN_ADDRESS_LINE_2_EXAMPLES = {
+            "101동 1204호", "가나아파트 302동 501호", "행복빌라 203호", "사무실 A동 301호",
+            "지하 1층", "옥탑방", "별관 2층", "본관 10층", "102동 2001호", "다성빌라 101호",
+            "현대타워 1503호", "7층", "201호", "사랑채", "별관 사무동 301호", "12층 스카이라운지",
+            "A블록 101동 101호", "B상가 205호", "지하상가 13호", "연구동 404호", "강의동 502호",
+            "학생회관 301실", "2공학관 512호", "창조관 707호", "301호 (3층)", "1205호 (12층)"
+    };
 
     public List<Map<String, Object>> generateData(DataGenerationRequest request) {
         List<Map<String, Object>> generatedData = new ArrayList<>();
@@ -79,6 +105,7 @@ public class DataGenerationService {
             case "korean_city" -> koreanFaker.address().city();
             case "korean_state" -> koreanFaker.address().state();
             case "korean_postal_code" -> koreanFaker.address().zipCode();
+            case "korean_address_line_2" -> KOREAN_ADDRESS_LINE_2_EXAMPLES[defaultFaker.random().nextInt(KOREAN_ADDRESS_LINE_2_EXAMPLES.length)];
 
             // --- [ENGLISH] Address ---
             case "address" -> defaultFaker.address().fullAddress();
@@ -87,6 +114,7 @@ public class DataGenerationService {
             case "state" -> defaultFaker.address().state();
             case "country" -> defaultFaker.address().country();
             case "postal_code" -> defaultFaker.address().zipCode();
+            case "address_line_2" -> ADDRESS_LINE_2_EXAMPLES[defaultFaker.random().nextInt(ADDRESS_LINE_2_EXAMPLES.length)];
 
             // --- [KOREAN] Company & Commerce ---
             case "korean_company_name" -> koreanFaker.company().name();
